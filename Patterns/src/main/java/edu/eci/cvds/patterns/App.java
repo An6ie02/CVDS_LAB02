74a0098614efbc4d9981cffa130c7acd2c4de2cb
ELS4NTA <daniel1sa_@hotmail.com>
2023-02-12 00:10:21 -0500

    Proyecto Maven

--- Patterns/src/main/java/edu/eci/cvds/patterns/App.java
+++ Patterns/src/main/java/edu/eci/cvds/patterns/App.java
@@ -4,16 +4,17 @@ package edu.eci.cvds.patterns;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        if(args.length==0){
-            System.out.println( "Hello World!" );
+public class App {
+    public static void main( String[] args ) {
+        String message = "Hello";
+        if (args.length == 0) {
+            message += " World";
+        } else {
+            for (String word: args) {
+                message += " " + word;
+            }
         }
-        else{
-            System.out.println( "Hello " + args[0] + " " + args[1] + "!" );
-        }
-        
+        message += "!";
+        System.out.println(message);
     }
 }
